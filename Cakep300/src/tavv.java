8e608405aede9191d914c54449841ff984c50785
5112100209 TA. Muhammad Aunorafiq <raffi@kcv.if.its.ac.id>
2015-12-15 23:44:55 -0700

    done layer 1

--- Cakep300/src/tavv.java
+++ Cakep300/src/tavv.java
@@ -4,6 +4,7 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.io.File;
 import java.io.IOException;
+import static java.lang.Math.min;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
@@ -16,7 +17,6 @@ import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.imgcodecs.Imgcodecs;
-import static org.opencv.imgproc.Imgproc.equalizeHist;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -36,23 +36,25 @@ public final class tavv extends javax.swing.JFrame {
      */
     public tavv() throws IOException {
         initComponents();
+        final int kernelTopHat = 15;
+        final int kernelDilasi = 6;
         
-        Mat greyscale = Imgcodecs.imread("C:\\Users\\raffi\\Documents\\TA\\tavv\\imagemalam2\\SSPadalarang2.jpg",Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
-        Mat ori = Imgcodecs.imread("C:\\Users\\raffi\\Documents\\TA\\tavv\\imagemalam2\\SSPadalarang2.jpg");
+        Mat greyscale = Imgcodecs.imread("C:\\Users\\raffi\\Documents\\TA\\tavv\\imagemalam2\\BuahBatuEntrance1.jpg",Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
+        Mat ori = Imgcodecs.imread("C:\\Users\\raffi\\Documents\\TA\\tavv\\imagemalam2\\BuahBatuEntrance1.jpg");
         
         // grayscale
         jLabel1.setIcon(new ImageIcon(toBufferedImage(ori))); 
         jLabel2.setIcon(new ImageIcon(toBufferedImage(greyscale)));
         
         // top hat
-        Mat kernel = Mat.ones(13, 13, CvType.CV_8UC1);
+        Mat kernel = Mat.ones(kernelTopHat, kernelTopHat, CvType.CV_8UC1);
         Mat tophat = new Mat();
         Imgproc.morphologyEx(greyscale, tophat, Imgproc.MORPH_TOPHAT, kernel);
         jLabel3.setIcon(new ImageIcon(toBufferedImage(tophat)));
         
         // otsu + open
-        Mat kernel2 = Mat.ones(5, 5, CvType.CV_8UC1);
-        Mat kernel3 = Mat.ones(5, 5, CvType.CV_8UC1);
+        Mat kernel2 = Mat.ones(kernelDilasi, kernelDilasi, CvType.CV_8UC1);
+        Mat kernel3 = Mat.ones(kernelDilasi, kernelDilasi, CvType.CV_8UC1);
         Mat otsu = new Mat();
         Imgproc.threshold(tophat, otsu, 0, 255, Imgproc.THRESH_OTSU);
         Imgproc.morphologyEx(otsu, otsu, Imgproc.MORPH_OPEN, kernel2);
@@ -67,7 +69,6 @@ public final class tavv extends javax.swing.JFrame {
 //        System.out.println(centroids.dump());
 //        System.out.println(stats.dump());
         
-               
         // get data centroid
         int a1;
         centroids.convertTo(centroids, CvType.CV_32S);
@@ -90,7 +91,7 @@ public final class tavv extends javax.swing.JFrame {
         // left_most; top_most; widht; height; area;
         int size_stats = (int)stats.total() * stats.channels();
         int[] data_stats = new int[size_stats];
-        int[][] data_stats2 = new int[size_stats/5][size_stats];
+        int[][] dataStats2 = new int[size_stats/5][size_stats];
         a1 = stats.get(0, 0, data_stats);
         System.out.println(a1);
         int y=0;
@@ -98,23 +99,23 @@ public final class tavv extends javax.swing.JFrame {
             switch (i%5) {
                 case 0:
                     System.out.print("x : " + data_stats[i] +" ");
-                    data_stats2[y][0] = data_stats[i];
+                    dataStats2[y][0] = data_stats[i];
                     break;
                 case 1:
                     System.out.print("y : " + data_stats[i] +" ");
-                    data_stats2[y][1] = data_stats[i];
+                    dataStats2[y][1] = data_stats[i];
                     break;
                 case 2:
                     System.out.print("w : " + data_stats[i] +" ");
-                    data_stats2[y][2] = data_stats[i];
+                    dataStats2[y][2] = data_stats[i];
                     break;
                 case 3:
                     System.out.print("h : " + data_stats[i] +" ");
-                    data_stats2[y][3] = data_stats[i];
+                    dataStats2[y][3] = data_stats[i];
                     break;
                 case 4:
                     System.out.println("a : " + data_stats[i] );
-                    data_stats2[y][4] = data_stats[i];
+                    dataStats2[y][4] = data_stats[i];
                     y++;
                     break;
                 default:
@@ -127,40 +128,90 @@ public final class tavv extends javax.swing.JFrame {
 //        Imgproc.rectangle(ori, new Point(data_stats2[i][0],data_stats2[i][1]), new Point(data_stats2[i][0]+data_stats2[i][2],data_stats2[i][1]+data_stats2[i][3]), new Scalar(0, 255, 255), 2);
 //        jLabel5.setIcon(new ImageIcon(toBufferedImage(ori)));
 
+        Mat gambarIni = ori.clone();
+        Rect[] arrRectCrop = new Rect[10000];
+        int jlhKandidat = 0;
+        int batasBawah, batasKanan, widthRoi, heightRoi, xRoi, yRoi;
         
-        Rect[] arrRectCrop = new Rect[100];
-        
-        int jlh_kandidat = 0;
-        int tes1 = 0, tes2 = 0;
         for(int i=1;i<jlh_obj;i++){
+            if(dataStats2[i][2] >= 20 ){
+                batasBawah = dataStats2[i][1] + dataStats2[i][3];
+                batasKanan = dataStats2[i][0] + dataStats2[i][2];
+                
+                widthRoi = dataStats2[i][2] ;
+                heightRoi = dataStats2[i][3] ;
+                xRoi = dataStats2[i][0];
+                yRoi = batasBawah - widthRoi;
+                if(yRoi < 0) break;
+
+                // retangle(image, point kiri atas, point kanan bawah, warna, tebal
+
+                Imgproc.rectangle(gambarIni,
+                        new Point(xRoi, yRoi),
+                        new Point(batasKanan, batasBawah),
+                        new Scalar(255, 0, 255),
+                        1);
+                
+                Rect tempCrop = new Rect(xRoi, yRoi, widthRoi, widthRoi);
+
+                arrRectCrop[jlhKandidat] = tempCrop;
+                jlhKandidat++;
+//                System.out.println(jlhKandidat);
+                
+            }
             for(int j=1;j<jlh_obj;j++){
-                if(i==j || data_centroids2[i][0] > data_centroids2[j][0]) continue;
-                if(Math.abs(data_centroids2[i][0] - data_centroids2[j][0]) < 50){
-                    if(Math.abs(data_centroids2[j][1] - data_centroids2[i][1]) < 10){
-                        Imgproc.rectangle(ori, new Point(data_stats2[i][0],data_stats2[i][1]), new Point(data_stats2[j][0]+data_stats2[j][2], data_stats2[j][1]+data_stats2[j][3]), new Scalar(255, 0, 255), 2);
-                        Rect tempCrop = new Rect(data_stats2[i][0], data_stats2[i][1], (data_stats2[j][0]+data_stats2[j][2]) -  data_stats2[i][0], (data_stats2[j][1]+data_stats2[j][3]) - data_stats2[i][1]);
-                        arrRectCrop[jlh_kandidat] = tempCrop;
-                        jlh_kandidat++;
-                        tes1 = i; tes2 = j;
-                    }
+                int jarakX = data_centroids2[i][0] - data_centroids2[j][0];
+                int jarakY = data_centroids2[j][1] - data_centroids2[i][1];
+                
+                if(i==j || data_centroids2[i][0] > data_centroids2[j][0]
+                        || dataStats2[i][4] > 300
+                        || dataStats2[j][4] > 300) continue;
+//                if(dataStats2[i][4] > 300) System.out.println(dataStats2[i][4]);
+                if(Math.abs(jarakX) < 50 && Math.abs(jarakY) < 10){
+                    int titikTerendahI = dataStats2[i][1] + dataStats2[i][3];
+                    int titikTerendahJ = dataStats2[j][1] + dataStats2[j][3];
+                    batasBawah = min(titikTerendahI, titikTerendahJ);
+                    batasKanan = dataStats2[j][0] + dataStats2[j][2];
+                    
+                    widthRoi = batasKanan - dataStats2[i][0] ;
+                    heightRoi = widthRoi ;
+                    xRoi = dataStats2[i][0];
+                    yRoi = batasBawah - widthRoi;
+                    if(yRoi < 0) break;
+                    
+                    
+                    // retangle(image, point kiri atas, point kanan bawah, warna, tebal
+                    Imgproc.rectangle(gambarIni,
+                            new Point(xRoi, yRoi), 
+                            new Point(batasKanan, batasBawah), 
+                            new Scalar(255, 0, 255),
+                            1);
+
+
+                    // rect(x, y, widht, height)
+//                    Rect tempCrop = new Rect(dataStats2[i][0],
+//                            dataStats2[i][1],
+//                            (dataStats2[j][0]+dataStats2[j][2]) - dataStats2[i][0],
+//                            (dataStats2[j][1]+dataStats2[j][3]) - dataStats2[i][1]);
+                    Rect tempCrop = new Rect(xRoi, yRoi, widthRoi, heightRoi);
+                    
+                    arrRectCrop[jlhKandidat] = tempCrop;
+                    jlhKandidat++;
+                    
                 }
             }
         }
-        jLabel5.setIcon(new ImageIcon(toBufferedImage(ori)));
-        System.out.println(jlh_kandidat);
+        jLabel5.setIcon(new ImageIcon(toBufferedImage(gambarIni)));
+        System.out.println(jlhKandidat);
         
-        //Rect rectCrop = new Rect(data_stats2[tes1][0], data_stats2[tes1][1], (data_stats2[tes2][0]+data_stats2[tes2][2]) -  data_stats2[tes1][0], (data_stats2[tes2][1]+data_stats2[tes2][3]) - data_stats2[tes1][1]);
-        
-//        Mat imCrop = new Mat(ori, arrRectCrop[0]);
-//        jLabel6.setIcon(new ImageIcon(toBufferedImage(imCrop)));
-        
-//        BufferedImage output =  (BufferedImage) toBufferedImage(imCrop);
-//        File outputFile = new File("image.jpg");
-//        ImageIO.write(output, "jpg", outputFile);
+                
+        Mat imCrop = new Mat(ori, arrRectCrop[3]);
+        jLabel6.setIcon(new ImageIcon(toBufferedImage(imCrop)));
+      
+        BufferedImage output =  (BufferedImage) toBufferedImage(imCrop);
+        File outputFile = new File("image.jpg");
+        ImageIO.write(output, "jpg", outputFile);
 
-//        Mat histo = new Mat();
-//        equalizeHist(greyscale, histo);
-//        jLabel6.setIcon(new ImageIcon(toBufferedImage(histo)));
     }
 
     /**
@@ -273,6 +324,8 @@ public final class tavv extends javax.swing.JFrame {
         System.arraycopy(b, 0, targetPixels, 0, b.length);
         return image;
     }
+    
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
